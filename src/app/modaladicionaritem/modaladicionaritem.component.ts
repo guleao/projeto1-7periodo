d2a88ebe6925c8b06716824a451d3e988c4c671e
guleao <guztavusz@gmail.com>
2025-02-12 19:19:58 -0300

    adiciona funcionalidade de modal para adicionar itens à lista de compras e atualiza a exibição da tabela

--- src/app/modaladicionaritem/modaladicionaritem.component.ts
+++ src/app/modaladicionaritem/modaladicionaritem.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modaladicionaritem',
-  imports: [],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './modaladicionaritem.component.html',
   styleUrl: './modaladicionaritem.component.css'
 })
 export class ModaladicionaritemComponent {
+  @Output() itemAdicionado = new EventEmitter<{ nome: string, quantidade: string }>();
+  nome = '';
+  quantidade = '';
 
+  adicionarItem() {
+    this.itemAdicionado.emit({ nome: this.nome, quantidade: this.quantidade });
+    this.nome = '';
+    this.quantidade = '';
+  }
 }
